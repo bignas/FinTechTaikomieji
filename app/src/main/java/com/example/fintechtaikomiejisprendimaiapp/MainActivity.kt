e988f92084342abc4c0127c1b31f766eaa673bb2
Ignas <ignasbuinickas@gmail.com>
2024-03-11 23:58:38 +0200

    Revert "Added button for color change"
    This reverts commit fdb65ca9739dfdccf761791e7b163589a5b2c347.

--- app/src/main/java/com/example/fintechtaikomiejisprendimaiapp/MainActivity.kt
+++ app/src/main/java/com/example/fintechtaikomiejisprendimaiapp/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.fintechtaikomiejisprendimaiapp
 
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.*
 import androidx.activity.ComponentActivity
-import java.util.Random
-
 
 class MainActivity : ComponentActivity() {
     lateinit var textView: TextView
     lateinit var button: Button
-    lateinit var buttonChangeColor: Button
-    val random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +14,12 @@ class MainActivity : ComponentActivity() {
 
         textView = findViewById(R.id.text_view)
         button = findViewById(R.id.button)
-        buttonChangeColor = findViewById(R.id.button_change_color)
 
         var num = 1
+
         button.setOnClickListener {
             textView.text = num.toString()
             num += 1
-        }
-
-        buttonChangeColor.setOnClickListener {
-            val red = random.nextInt(256)
-            val green = random.nextInt(256)
-            val blue = random.nextInt(256)
-            textView.setTextColor(Color.argb(255, red, green, blue))
         }
     }
 }
